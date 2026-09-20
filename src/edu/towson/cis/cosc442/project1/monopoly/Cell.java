93b0a5cc3c10aeada1d3f6bd8cf704937ca6d4a5
GavinLabino7 <gavinlabino@gmail.com>
2026-09-20 10:41:13 -0400

    Task 10 - Refactored the for loop in the getrent method in the propertycell file

--- src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -1,44 +1,66 @@
 package edu.towson.cis.cosc442.project1.monopoly;
-/**
- * 
- * Cell
- */
+
+
 public abstract class Cell {
 	private boolean available = true;
 	private String name;
 	protected Player theOwner;
 
+	/** 
+	 * @return String
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/** 
+	 * @return Player
+	 */
 	public Player getTheOwner() {
 		return theOwner;
 	}
 	
+	/** 
+	 * @return int
+	 */
 	public int getPrice() {
 		return 0;
 	}
 
+	/** 
+	 * @return boolean
+	 */
 	public boolean isAvailable() {
 		return available;
 	}
 	
 	public abstract void playAction();
 
+	/** 
+	 * @param available
+	 */
 	public void setAvailable(boolean available) {
 		this.available = available;
 	}
 	
+	/** 
+	 * @param name
+	 */
 	void setName(String name) {
 		this.name = name;
 	}
 
+	/** 
+	 * @param owner
+	 */
 	public void setTheOwner(Player owner) {
 		this.theOwner = owner;
 	}
     
-    public String toString() {
+    /** 
+	 * @return String
+	 */
+	public String toString() {
         return name;
     }
 }
